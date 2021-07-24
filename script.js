78a2bf863d3583c72c2350308025bbc554f254ae
bbcoelho <bbcoelho@gmail.com>
2021-07-23 21:46:10 -0300

    modificado para o Snake Game dos antigos Nokia

--- script.js
+++ script.js
@@ -1,36 +1,40 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
-let box = 32;
+let box = 8;
 let snake = [];
 snake[0] ={
-    x: 8 * box,
-    y: 8 * box
+    x: 22*box,
+    y: 8*box
 }
 let direction = "right";
 let food ={
-    x: Math.floor(Math.random() * 15 + 1) * box,
-    y: Math.floor(Math.random() * 15 + 1) * box
+    x: Math.floor(Math.random() * 43 + 1) * box,
+    y: Math.floor(Math.random() * 29 + 1) * box
 }
 
 function criarBG(){
-    context.fillStyle = "gray";
-    context.fillRect(0, 0, 16*box, 16*box);
+    context.fillStyle = "rgb(188, 201, 0)";
+    context.fillRect(0, 0, 44*box, 30*box);
 }
 
-function criarCobrinha (){
+function criarCobrinha(){
     for(i = 0; i < snake.length; i++){
-        context.fillStyle = "black";
+        context.fillStyle = "rgb(111, 96, 0)";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
 
-function drawFood (){
-    context.fillStyle = "red";
+function drawFood(){
+    context.fillStyle = "black";
     context.fillRect(food.x, food.y, box, box);
 }
 
-document.addEventListener('keydown', update);
+function gameOver(){
+    clearInterval(jogo);
+    alert('Game Over!! :(  Recarregue a página para jogar novamente');
+}
 
+document.addEventListener('keydown', update);
 function update(event){
     if(event.keyCode == 37 && direction != 'right') direction = 'left';
     if(event.keyCode == 38 && direction != 'down') direction = 'up';
@@ -38,16 +42,16 @@ function update(event){
     if(event.keyCode == 40 && direction != 'up') direction = 'down';
 }
 
-function iniciarJogo(){    
-    if(snake[0].x > 15*box && direction == "right") snake[0].x = 0;
-    if(snake[0].x < 0 && direction == 'left') snake[0].x = 16 * box;
-    if(snake[0].y > 15*box && direction == "down") snake[0].y = 0;
-    if(snake[0].y < 0 && direction == 'up') snake[0].y = 16 * box;
+function iniciarJogo(){   
     
+    if(snake[0].x > 43*box && direction == "right") gameOver();
+    if(snake[0].x < 0 && direction == 'left') gameOver();
+    if(snake[0].y > 29*box && direction == "down") gameOver();
+    if(snake[0].y < 0 && direction == 'up') gameOver();
+
     for(i = 1; i < snake.length; i++){
         if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
-            clearInterval(jogo);
-            alert('Game Over :(');
+            gameOver();
         }
     }
 
@@ -60,16 +64,17 @@ function iniciarJogo(){
 
     if(direction == "right") snakeX += box;
     if(direction == "left") snakeX -= box;
-    if (direction == "up") snakeY -= box;
+    if(direction == "up") snakeY -= box;
     if(direction == "down") snakeY += box;
-
+    
     if(snakeX != food.x || snakeY != food.y){
         snake.pop();
-    }else{
-        food.x = Math.floor(Math.random() * 15 +1) * box;
-        food.y = Math.floor(Math.random() * 15 +1) * box;
     }
-    
+    else{
+        food.x = Math.floor(Math.random() * 43 +1) * box;
+        food.y = Math.floor(Math.random() * 29 +1) * box;
+    }
+
     let newHead ={
         x: snakeX,
         y: snakeY
@@ -78,4 +83,4 @@ function iniciarJogo(){
     snake.unshift(newHead);
 }
 
-let jogo = setInterval(iniciarJogo, 100);
+let jogo = setInterval(iniciarJogo, 70);
